3e4a1ba61c634324b1119018e952d0da615d6ab0
Shubham Biyani <shubhambiyani10@gmail.com>
2023-04-07 13:00:23 +0530

    team id of vercel

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
       apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
       appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-      // teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
       roles: ["strapi-super-admin"],
     },
   },
